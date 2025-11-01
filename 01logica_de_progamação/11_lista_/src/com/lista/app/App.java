b3bcf33870d79d18e85abd25ec0d34d85299b800
carlao-1208 <carlosbob27@gmail.com>
2025-10-31 21:47:07 -0300

    aula_do_dia_31_10_2025

---
+++ 01logica_de_progamação/11_lista_/src/com/lista/app/App.java
@@ -0,0 +1,29 @@
+package com.lista.app;
+
+import java.util.Scanner;
+
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        //instacia a classe Scanner
+        Scanner leia = new Scanner(System.in); //Scanner
+        String[] nomes = new String[10]; //array
+
+        // Entrada de dados
+        for (int i = 0; i < nomes.length; i++) {
+            System.out.println("Informe o " + (i+1) + " º nome" );
+            nomes[i] = leia.nextLine();
+        }
+
+        //saída de dados
+        for (String nome : nomes) {
+            System.out.println(nome);
+        }
+
+
+
+        //fecha o objeto leia
+        leia.close();
+        
+    }
+}
