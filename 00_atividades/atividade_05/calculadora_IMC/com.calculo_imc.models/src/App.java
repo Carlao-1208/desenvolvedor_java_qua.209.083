cad0c608bbc0d295407be37d43daa2326a62bd03
carlao-1208 <carlosbob27@gmail.com>
2025-11-12 21:43:20 -0300

    exercicio_calc_IMC

---
+++ 00_atividades/atividade_05/calculadora_IMC/com.calculo_imc.models/src/App.java
@@ -0,0 +1,29 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        //atributos
+    private double peso;
+    private double altura;
+
+    public peso() {
+    }
+
+    public double getPeso() {
+        return this.peso;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    public double getAltura() {
+        return this.altura;
+    }
+
+    public void setAtura(double altura) {
+        this.altura = altura;
+    }
+
+    public String calcularImcString() {
+        return (this.etanol >= this.gasolina*0.7) ? "Melhor abastecer com gasolina." : "Melhor abastecer com etanol.";
+    }
+}
